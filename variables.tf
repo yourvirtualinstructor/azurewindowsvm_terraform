0a9fa87c5bd07c9ff950b5342aceb40420a03b41
yourvirtualinstructor <145182304+yourvirtualinstructor@users.noreply.github.com>
2023-09-16 11:21:17 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "rg_name" {
+  description = "resoruce group name"
+  type        = string
+}
+
+variable "rg_location" {
+  description = "resoruce group location"
+  type        = string
+}
