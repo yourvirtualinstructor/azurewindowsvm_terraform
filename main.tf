df0432b8cb144206c8eee70560a1b1ab0f96983a
yourvirtualinstructor <145182304+yourvirtualinstructor@users.noreply.github.com>
2023-09-16 11:19:31 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,44 @@
+provider "azurerm" {
+  features {
+    api_management {
+      purge_soft_delete_on_destroy = true
+      recover_soft_deleted         = false
+    }
+
+    application_insights {
+      disable_generated_rule = true
+    }
+
+    cognitive_account {
+      purge_soft_delete_on_destroy = true
+    }
+
+    log_analytics_workspace {
+      permanently_delete_on_destroy = true
+    }
+
+    managed_disk {
+      expand_without_downtime = true
+    }
+
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+
+    template_deployment {
+      delete_nested_items_during_deletion = true
+    }
+
+    virtual_machine {
+      delete_os_disk_on_deletion     = true
+      graceful_shutdown              = false
+      skip_shutdown_and_force_delete = true
+    }
+
+    virtual_machine_scale_set {
+      force_delete                  = false
+      roll_instances_when_required  = true
+      scale_to_zero_before_deletion = true
+    }
+  }
+}
